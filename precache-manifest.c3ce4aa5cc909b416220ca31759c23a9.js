ce96469a1a41f7ddcbe1f38ba7baf01ded7a175c
develSanx <develsanx@gmail.com>
2019-09-14 20:35:52 +0530

    added my work

---
+++ precache-manifest.c3ce4aa5cc909b416220ca31759c23a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbb822c5453fc2d60f85d176f007d67",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "439910f2fafa2bc39d11",
+    "url": "/portfolio/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "0d6d0fd82bd80f4f8dd3",
+    "url": "/portfolio/static/css/main.9e092f50.chunk.css"
+  },
+  {
+    "revision": "439910f2fafa2bc39d11",
+    "url": "/portfolio/static/js/2.2bf29269.chunk.js"
+  },
+  {
+    "revision": "0d6d0fd82bd80f4f8dd3",
+    "url": "/portfolio/static/js/main.33c09ae0.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "5d166b19cfc326f19d998bb161d8b1db",
+    "url": "/portfolio/static/media/profile.5d166b19.jpg"
+  },
+  {
+    "revision": "005c6e64c0ce08caaab0657d57425057",
+    "url": "/portfolio/static/media/proj1.005c6e64.jpg"
+  },
+  {
+    "revision": "a032463409552e6795beda92ce01b182",
+    "url": "/portfolio/static/media/proj2.a0324634.jpg"
+  },
+  {
+    "revision": "a92c7a55b3716958def4f2d12e25f718",
+    "url": "/portfolio/static/media/proj3.a92c7a55.jpg"
+  }
+]);
